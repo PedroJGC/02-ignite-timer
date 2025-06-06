2b13dc04ee0ef3bc9de4118148d95dc12a88bdd4
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-05 22:45:45 -0300

    feat: Update homepage URL in package.json and set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { CyclesContextProvider } from './contexts/CyclesContext'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/02-ignite-timer">
         <CyclesContextProvider>
           <Router />
         </CyclesContextProvider>
